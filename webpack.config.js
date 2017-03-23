23f677be41269bd0a188ba72aa1a5f6f6459d413
Will-Wrightwa <wawwright@gmail.com>
2017-03-23 15:49:29 -0500

    added babel loader to webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+
+  context: path.join(__dirname, 'src'),
+  entry:{
+    app: './app/app.js'
+  },
+
+  output:{
+    path: path.join(__dirname , 'dist'),
+    filename: '[name].bundle.js'
+  },
+
+  module:{
+    loaders: [
+      {
+        test:/\.js$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src')
+      }
+
+    ]
+  }
+};
